7d6fdaf89f89e17f3f2e0708d79b8649a92a0907
Aman-036 <aman18618587@gmail.com>
2026-03-26 21:20:16 +0530

    Create Edge.java

---
+++ src/Edge.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Edge {
+    String destination;
+    int weight;
+
+    Edge(String dest, int w) {
+        this.destination = dest;
+        this.weight = w;
+    }
+}
